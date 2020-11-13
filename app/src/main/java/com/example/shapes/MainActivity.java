0206e2975bb77eefdfe0fd4f04422acd5aaaba64
haschish <haschish@gmail.com>
2020-11-13 20:33:55 +0500

    простая отрисовка прямоугольника и круга

--- app/src/main/java/com/example/shapes/MainActivity.java
+++ app/src/main/java/com/example/shapes/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.shapes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 public class MainActivity extends AppCompatActivity {
+    RadioGroup shapes;
+    Scene scene;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scene = findViewById(R.id.scene);
+        shapes = findViewById(R.id.shapeGroup);
+        shapes.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rect: scene.setTypeShape("rect"); break;
+                    case R.id.circle: scene.setTypeShape("circle"); break;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/shapes/Scene.java
+++ app/src/main/java/com/example/shapes/Scene.java
@@ -7,6 +7,7 @@ import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -15,10 +16,19 @@ import androidx.annotation.Nullable;
 public class Scene extends View {
     Point[] points = new Point[3];
     float density;
-    int gridSize = 20;
+    int gridSize = 30;
     int gridWidth;
     int gridHeight;
     int countPoints;
+    String mode = "draw";
+    private String typeShape = "rect";
+
+    Point corner = null;
+    int width;
+    int height;
+
+    Point center = null;
+    int radius;
 
     public Scene(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -38,12 +48,34 @@ public class Scene extends View {
 
         drawGrid(canvas);
         drawPoints(canvas);
+        drawRect(canvas);
+        drawCircle(canvas);
+    }
+
+    private void drawCircle(Canvas canvas) {
+        if (center != null) {
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(Color.BLACK);
+            paint.setStrokeWidth(density);
+            canvas.drawCircle(center.x, center.y, radius, paint);
+        }
+    }
+
+    private void drawRect(Canvas canvas) {
+        if (corner != null) {
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(Color.BLACK);
+            paint.setStrokeWidth(density);
+            canvas.drawRect(corner.x, corner.y, corner.x + width, corner.y + height, paint);
+        }
     }
 
     private void drawGrid(Canvas canvas) {
         Paint paint = new Paint();
         paint.setStyle(Paint.Style.STROKE);
-        DashPathEffect effects = new DashPathEffect(new float[] { 30, 10}, 0);
+        DashPathEffect effects = new DashPathEffect(new float[] { 3, 9}, 0);
         paint.setPathEffect(effects);
 
         for (int i = 0; i < gridWidth ; i++) {
@@ -72,18 +104,63 @@ public class Scene extends View {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            addPoint((int)event.getX(), (int)event.getY());
+            onDownTouch((int)event.getX(), (int)event.getY());
         }
         return true;
     }
 
-    private void addPoint(int x, int y) {
-        if (countPoints >= 3) {
-            return;
+    private void onDownTouch(int x, int y) {
+        if (this.mode.equals("draw")) {
+            Log.i("onDownTouch", typeShape);
+            switch (this.typeShape) {
+                case "rect": addRectPoint(x, y); break;
+                case "circle": addCirclePoint(x, y); break;
+            }
         }
+    }
 
+    private void addCirclePoint(int x, int y) {
         points[countPoints] = new Point(x, y);
         countPoints += 1;
+        if (countPoints >= 2) {
+            int radius = points[1].x - points[0].x;
+            createCircle(new Point(points[0]), radius);
+            countPoints = 0;
+        }
+        invalidate();
+    }
+
+    private void createCircle(Point center, int radius) {
+        this.center = center;
+        this.radius = radius;
+        invalidate();
+    }
+
+    private void addRectPoint(int x, int y) {
+        points[countPoints] = new Point(x, y);
+        countPoints += 1;
+        if (countPoints >= 3) {
+            int width = points[1].x - points[0].x;
+            int height = points[2].y - points[0].y;
+            createRect(new Point(points[0]), width, height);
+            countPoints = 0;
+        }
+        invalidate();
+    }
+
+    private void createRect(Point corner, int width, int height) {
+        this.corner = corner;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public void setTypeShape(String type) {
+        this.typeShape = type;
+        countPoints = 0;
         invalidate();
     }
 }
